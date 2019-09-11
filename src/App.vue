3386c33d01b637d7fb41b3a6ecc966fa89b42f4a
Justin Navarro <info@justinnavarro.net>
2019-09-11 13:37:36 -0400

    update the look of app vue fixed tests to detect vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-	<div id="app">
-		<v-app>
-			<router-view></router-view>
-		</v-app>
-	</div>
+	<v-app>
+		<v-app-bar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Heroes</span>
+			</v-toolbar-title>
+		</v-app-bar>
+
+		<v-content style="margin-bottom: 2rem;">
+			<v-layout justify-center>
+				<v-flex lg8 xl8>
+					<router-view/>
+				</v-flex>
+			</v-layout>
+		</v-content>
+	</v-app>
 </template>
 
 <script lang="ts">
---
+++ tests/unit/app.spec.ts
@@ -0,0 +1,21 @@
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App.vue';
+import Vue from 'vue';
+import vuetify from 'vuetify';
+import Router from 'vue-router';
+
+
+describe('App.vue', () => {
+    let wrapper: any;
+
+    beforeEach(() => {
+        Vue.use(vuetify);
+        Vue.use(Router);
+
+        wrapper = shallowMount(App, {});
+    });
+
+    it('renders props.msg when passed', () => {
+        expect(wrapper.isVueInstance()).toBeTruthy();
+    });
+});
