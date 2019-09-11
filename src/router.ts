95969dac94861b0dd9e9ce8ce4eeda5579777594
Justin Navarro <info@justinnavarro.net>
2019-09-11 13:40:08 -0400

    dispatch action before going to home page

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import store from '@/store/index';
+import Router, {Route} from 'vue-router';
 
 Vue.use(Router);
 
@@ -8,9 +9,13 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
         {
+            beforeEnter: (to: Route, from: Route, next: any) => {
+                store.dispatch('Heroes/getHeroes');
+                next();
+            },
             component: () => import(/* webpackChunkName: "home" */ '@/pages/Home.vue'),
             name: 'home',
             path: '/',
-        }
+        },
     ],
 });
