cfa9d289d37a81dae5d52ba686049db6548d77ba
Justin Navarro <info@justinnavarro.net>
2019-09-11 21:24:11 -0400

    build out the search component and tests

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,13 +1,112 @@
 <template>
 	<div id="search">
-		Search
+		<v-layout class="search-container" justify-center>
+			<v-flex lg8 xl8>
+				<v-layout justify-space-between>
+					<v-flex lg3 xl3>
+						<v-text-field
+							v-model="form.name"
+							label="Search Hero by Name"></v-text-field>
+					</v-flex>
+
+					<v-flex lg3 xl3>
+						<v-select
+							v-model="form.universe"
+							:items="universeList"
+							label="Select Universe"></v-select>
+					</v-flex>
+
+					<v-flex lg3 xl3>
+						<v-select
+							v-model="form.gender"
+							:items="genderList"
+							label="Select Gender"></v-select>
+					</v-flex>
+				</v-layout>
+
+				<v-layout justify-center>
+					<v-btn
+						:class="{'disabledBtn': !canSubmit()}"
+						@click="resetFilter()"
+						:disabled="!canSubmit()">Reset</v-btn>
+					<v-btn
+						class="submit-btn"
+						:class="{'disabledBtn': !canSubmit()}"
+						@click="submitFilter()"
+						color="green"
+						dark
+						:disabled="!canSubmit()"
+						style="margin-left: .875rem;">
+						Submit
+					</v-btn>
+				</v-layout>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
+	import {mapGetters, mapMutations} from 'vuex';
 
-	export default Vue.extend({});
+	export default Vue.extend({
+		computed: {
+			...mapGetters('Heroes', [
+				'genderList',
+				'universeList',
+			]),
+		},
+		data: () => {
+			return {
+				form: {
+					name: '',
+					gender: '',
+					universe: '',
+				},
+			};
+		},
+		methods: {
+			...mapMutations('Heroes', [
+				'SET_FILTER_GENDER',
+				'SET_FILTER_NAME',
+				'SET_FILTER_UNIVERSE',
+			]),
+			canSubmit(): boolean {
+				return this.isNotEmpty(this.form.name) ||
+					this.isNotEmpty(this.form.gender) ||
+					this.isNotEmpty(this.form.universe);
+			},
+			isNotEmpty(value: string): boolean {
+				return !!value && !!value.trim();
+			},
+			resetFilter(): void {
+				this.form = {
+					name: '',
+					gender: '',
+					universe: '',
+				};
+				this.submitFilter();
+			},
+			submitFilter(): void {
+				this.SET_FILTER_GENDER(this.form.gender);
+				this.SET_FILTER_NAME(this.form.name);
+				this.SET_FILTER_UNIVERSE(this.form.universe);
+			},
+		},
+	});
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+	#search {
+		.search-container {
+			background: #fcfcfc;
+			border-bottom: 1px solid #ededed;
+			padding: 1rem 0;
+		}
+
+		.disabledBtn {
+			background: #ccc !important;
+			color: #aaa !important;
+		}
+	}
+</style>
---
+++ tests/unit/components/Search.spec.ts
@@ -0,0 +1,173 @@
+import {createLocalVue, shallowMount} from '@vue/test-utils';
+import Search from '@/components/Search.vue';
+import Vue from 'vue';
+import vuetify from 'vuetify';
+import HeroesState from '@/store/modules/heroes/heroes.state';
+import HeroesActions from '@/store/modules/heroes/heroes.action';
+import Vuex from 'vuex';
+import HeroesMutations from '@/store/modules/heroes/heroes.mutation';
+import HeroesGetters from '@/store/modules/heroes/heroes.getter';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const heroData = [
+    {
+        name: 'Jasmine',
+        alias: 'Jasmine Test',
+        image: '',
+        description: 'This is from a test.',
+        universe: 'Angular testing universe',
+        gender: 'Female',
+        species: 'jasmine',
+    },
+    {
+        name: 'Jest',
+        alias: 'Jest Test',
+        image: '',
+        description: 'This is from a test.',
+        universe: 'Vue testing universe',
+        gender: 'Male',
+        species: 'jest',
+    },
+    {
+        name: 'Mocha',
+        alias: 'Mocha Test',
+        image: '',
+        description: 'This is from a test.',
+        universe: 'Javascript testing universe',
+        gender: 'Male',
+        species: 'mocha',
+    },
+];
+
+jest.mock('@/shared/services/heroes.service', () => ({
+    getHeroes: jest.fn(() => {
+        return new Promise((resolve) => {
+            (process as any).nextTick(() => {
+                resolve({
+                    status: 200,
+                    data: {
+                        data: heroData,
+                    },
+                });
+            });
+        });
+    }),
+}));
+
+describe('Search.vue', () => {
+    let store: any;
+    let wrapper: any;
+
+    beforeEach(() => {
+        Vue.use(vuetify);
+        store = new Vuex.Store({
+            modules: {
+                Heroes: {
+                    namespaced: true,
+                    state: HeroesState,
+                    getters: HeroesGetters,
+                    mutations: HeroesMutations,
+                    actions: HeroesActions,
+                },
+            },
+        });
+        wrapper = shallowMount(Search, {
+            localVue,
+            store,
+        });
+    });
+
+    afterEach(() => {
+        store.commit('Heroes/RESET_HEROES');
+        store.commit('Heroes/SET_FILTER_GENDER', '');
+        store.commit('Heroes/SET_FILTER_NAME', '');
+        store.commit('Heroes/SET_FILTER_UNIVERSE', '');
+    });
+
+    it('renders without errors', () => {
+        expect(wrapper.isVueInstance()).toBeTruthy();
+    });
+
+    it('should filter the state based on name', async () => {
+        expect(store.getters['Heroes/heroesList'].length).toEqual(0);
+        await store.dispatch('Heroes/getHeroes');
+        store.commit('Heroes/SET_FILTER_NAME', 'Jasmine');
+        expect(store.getters['Heroes/heroesList'].length).toEqual(1);
+    });
+
+    it('should filter the state based on form input', async () => {
+        await store.dispatch('Heroes/getHeroes');
+
+        const form = {
+            gender: 'Male',
+            name: '',
+            universe: '',
+        };
+
+        wrapper.setData({
+            form,
+        });
+
+        wrapper.vm.submitFilter();
+        expect(store.state.Heroes.filter.gender).toEqual(form.gender);
+        expect(store.getters['Heroes/heroesList'].length).toBe(2);
+    });
+
+    it('should reset the filter form', async () => {
+        await store.dispatch('Heroes/getHeroes');
+
+        const form = {
+            gender: 'Male',
+            name: 'Mocha',
+            universe: 'Javascript testing universe',
+        };
+
+        wrapper.setData({
+            form,
+        });
+        wrapper.vm.resetFilter();
+        expect(store.state.Heroes.filter.gender).toEqual('');
+        expect(store.state.Heroes.filter.name).toEqual('');
+        expect(store.state.Heroes.filter.universe).toEqual('');
+    });
+
+    it('should check if the value is empty or not', () => {
+        expect(wrapper.vm.isNotEmpty('pizza')).toBeTruthy();
+        expect(wrapper.vm.isNotEmpty(undefined)).toBeFalsy();
+        expect(wrapper.vm.isNotEmpty(null)).toBeFalsy();
+        expect(wrapper.vm.isNotEmpty('')).toBeFalsy();
+    });
+
+    it('should check if button can be submitted', async () => {
+        await store.dispatch('Heroes/getHeroes');
+
+        expect(wrapper.vm.canSubmit()).toBeFalsy();
+        expect(wrapper.find('.submit-btn.disabledBtn').isVisible()).toBeTruthy();
+
+        const form = {
+            gender: '',
+            name: 'Jest',
+            universe: '',
+        };
+
+        wrapper.setData({
+            form,
+        });
+
+        expect(wrapper.vm.canSubmit()).toBeTruthy();
+        expect(wrapper.find('.submit-btn.disabledBtn').exists()).toBeFalsy();
+        expect(wrapper.find('.submit-btn').exists()).toBeTruthy();
+    });
+
+    it('should check if gender list is not empty', async () => {
+        await store.dispatch('Heroes/getHeroes');
+        expect(wrapper.vm.genderList.length).toBeGreaterThanOrEqual(1);
+    });
+
+    it('should check if universe list is not empty', async () => {
+        await store.dispatch('Heroes/getHeroes');
+        expect(wrapper.vm.universeList.length).toBeGreaterThanOrEqual(1);
+    });
+});
